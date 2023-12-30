d4e45ba7701fea4c2cbbb8339d0daec42b42e4fa
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2023-12-29 19:21:11 -0600

    Layout finished, function of obtaining categories, products by categories and loading spinner

--- src/assets/components/Products.jsx
+++ src/assets/components/Products.jsx
@@ -10,6 +10,7 @@ const Products = () =>{
     const [ProductsByCategory, setProductsByCategory] = useState([])
     const [Products, setProducts] = useState([])
     const [isLoading, setLoading] = useState(false)
+    // const []
 
     //cuando se renderize mi componenete pedire las categorias 
     useEffect(() =>{
@@ -105,16 +106,16 @@ const Products = () =>{
                                     </div>
                                    )) 
                             }
-                            <div>
-                                <button>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
-                                </svg>
+                            <div className="space-x-4">
+                                <button className="p-2 bg-purple-400 rounded-full">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
+                                    </svg>
                                 </button>
-                                <button>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                                </svg>
+                                <button className="p-2 bg-purple-400 rounded-full">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                                    </svg>
                                 </button>
                             </div>
                         </>
